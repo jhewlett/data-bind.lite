5f55d1c7e26c1bbf3fad361664387e46a126d80c
Justin Hewlett <hewlett.jus@gmail.com>
2014-03-30 22:01:35 -0600

    added test case

--- tests/binder-specs.js
+++ tests/binder-specs.js
@@ -8,6 +8,7 @@ describe('binder', function() {
 
         documentStub = sinon.stub({querySelector: function() {}});
         scopeElement = sinon.stub({querySelectorAll: function() {}});
+        scopeElement.querySelectorAll.withArgs('[data-bind]').returns([]);
         scopeElement.querySelectorAll.withArgs('[data-click]').returns([]);
         scopeElement.querySelectorAll.withArgs('[data-class]').returns([]);
         scopeElement.querySelectorAll.withArgs('[data-template]').returns([]);
@@ -16,6 +17,46 @@ describe('binder', function() {
     });
 
     describe('bind', function() {
+        describe('templates', function() {
+            var template;
+
+            describe('with invalid attribute name', function() {
+                beforeEach(function() {
+                    template = {innerHTML: 'Hello {{invalid}}'};
+                    scopeElement.querySelectorAll.withArgs('[data-template]').returns([template]);
+                    binder.bind();
+                });
+
+                it('should replace with empty string', function() {
+                    expect(template.innerHTML).toEqual('Hello ');
+                });
+            });
+
+            describe('with valid attribute name', function() {
+                beforeEach(function() {
+                    template = {innerHTML: 'Hello {{prop}}'};
+                    scopeElement.querySelectorAll.withArgs('[data-template]').returns([template]);
+                    binder.bind();
+                });
+
+                it('should replace html', function() {
+                    expect(template.innerHTML).toEqual('Hello myValue');
+                });
+
+                describe('changing the value again', function() {
+                    beforeEach(function() {
+                        scopeElement.querySelectorAll.withArgs('[data-class=prop]').returns([]);
+                        scopeElement.querySelectorAll.withArgs('[data-bind=prop]').returns([]);
+                    });
+
+                    it('should replace html with new value', function() {
+                        model.attr('prop', 'newValue');
+                        expect(template.innerHTML).toEqual('Hello newValue');
+                    });
+                });
+            });
+        });
+
         describe('has value property', function() {
             beforeEach(function() {
                 element = sinon.stub({getAttribute: function() {}, tagName: '', value: ''});
